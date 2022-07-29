415d75cb276f3b69e29e24d820ab286ddd0857e2
ChungyiBossi <chungyi.bossi@gmail.com>
2022-07-29 17:56:26 +0800

    [feature] Azure custom vision prediction call by restful api

---
+++ predict_with_restful.py
@@ -0,0 +1,24 @@
+import requests
+
+
+ENDPOINT = "YOUR_CUSTOM_VISION_ENDPOINT"
+prediction_key = "YOUR_CUSTOM_VISION_KEY"
+project_id = "YOUR_PROJECT_ID_INSETTING"
+iteration_name = "YOUR_MODEUL_PUBLISH_AS"
+
+
+headers = {
+    "Prediction-Key": prediction_key,
+    "Content-Type": "application/octet-stream"
+}
+
+
+for i in range(1, 4):
+    with open(f"./Test_data/test_img_{i}.jpg", "rb") as image_contents:
+        results = requests.post(
+            url=ENDPOINT, headers=headers, data=image_contents).json()
+
+        for pred in results['predictions']:
+            print(pred['tagName'], pred['probability'])
+
+        print("==========================")
